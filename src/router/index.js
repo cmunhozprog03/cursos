ba2bcb7f4594be2697d4c1fa67c91a62d8ccdf75
cmunhozprog03 <cmunhozprog03@hotmail.com>
2022-05-04 14:19:25 -0300

    protecer rotas

--- src/router/index.js
+++ src/router/index.js
@@ -30,12 +30,23 @@ const routes = [
   {
     path: '/dashboard',
     name: 'Dashboard',
-    component: () => import('../views/DashboardView.vue')
+    component: () => import('../views/DashboardView.vue'),
+    meta: {
+      requiresAuth: true
+    }
+    
   },
   {
     path: '/login',
     name: 'Login',
-    component: () => import('../views/auth/LoginView.vue')
+    component: () => import('../views/auth/LoginView.vue'),
+    beforeEnter: (to, from, next) => {
+      if (localStorage.getItem('auth')){
+        next('/dashboard')
+      } else {
+        next()
+      }
+    }
   },
   {
     path: '/register',
@@ -47,6 +58,16 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const protectedRoute = to.matched.some(record => record.meta.requiresAuth)
+
+  if(protectedRoute && !localStorage.getItem('auth') ){
+    next('/login')
+  } else{
+    next()
+  }
 })
 
 export default router
